5465a573fa84388f000ffe04587e8ab63a7d6ead
Luíza Furuno <luizafuruno@gmail.com>
2023-11-25 09:23:26 -0300

    Ajustando aa_list_%

--- lista_11/aa_list_percentage.py
+++ lista_11/aa_list_percentage.py
@@ -2,11 +2,22 @@
 def aas_percentage(aa_list, seq):
     seq_len = len(seq)
     percentages = []
-    for aminoacid in aa_list:
+    i = 0
+    while (i < len(aa_list)):
+        aminoacid = aa_list[i]
         count_aa = seq.upper().count(aminoacid.upper())
         percentage = (count_aa * 100) / seq_len
         percentages.append(percentage)
+        i += 1
     return percentages
-aa_list = input("Enter a list of aminoacids, to calculate their percentage in the protein: ").split(',')
-seq = input("Enter the protein sequence: ")
-print(aas_percentage(aa_list,seq))
+
+aa_list = ["R", "D", "G", "L"]
+seq = "RIKELDDRERQVSDELQDLLYQLPNLPAPDVPVGPDETGNVEVKRWGEPRQFAFPVKPHWDLGVAMDGLDFERAAKVTGS"
+result = aas_percentage(aa_list, seq)
+
+i = 0
+while i < len(aa_list):
+    aminoacid = aa_list[i]
+    percentage = result[i]
+    print(f'The percentage of the amino acid {aminoacid} is {percentage:.2f}%')
+    i += 1
